22542085dfa769b2adb31243d13a150c9609e571
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2019-06-18 03:15:57 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -6,30 +6,52 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.format.DateFormat;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private static final String TAG = "MainActivity";
     public TextView mDisplayDate;
-    public TextView mDisplayTime;
+    public TextView mDisplayTimeStart;
+    public TextView mDisplayTimeEnd;
     public DatePickerDialog.OnDateSetListener mDateListener;
     public TimePickerDialog.OnTimeSetListener mTimeListener;
+    public EditText date;
+    public TextWatcher tw;
+    public ListView listViewMovies;
+    private String current = "";
+    List<String> movielist = new ArrayList<>();
+    Theatre selectedTheatre;
     TextView temp;
     List<Theatre> spinnerArray = new ArrayList<>();
     Spinner spinner1;
     Context context = null;
+    int year;
+    int month;
+    int day;
+    int hourStart;
+    int minuteStart;
+    int minuteEnd;
+    int hourEnd;
 
 
     @Override
@@ -40,14 +62,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         final String url ="https://www.finnkino.fi/xml/TheatreAreas/";
         new XMLParser(this,url).execute();
+        doSpinner();
         doCalendar();
-        doClock();
-
-
+        doClockStart();
+        doClockEnd();
     }
+
+    public void doListViewMovies(){
+        listViewMovies = findViewById(R.id.listViewMovies);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, movielist);
+        listViewMovies.setAdapter(adapter);
+    }
+
+    public void getMovies() {
+
+        selectedTheatre = (Theatre) spinner1.getSelectedItem();
+        if (selectedTheatre != null) {
+            String movieurl = "http://www.finnkino.fi/xml/Schedule/?area=" + selectedTheatre.getID() + "&dt=" + current;
+            System.out.println(movieurl);
+            new XMLParserMovies(this, movieurl).execute();
+        }
+    }
+
     public void callBackData(ArrayList<Theatre> theatres) {
         spinnerArray = theatres;
         doSpinner();
+    }
+
+    public void callBackDataMovies(ArrayList<String> movies){
+        this.movielist = movies;
+        doListViewMovies();
     }
 
     public void doSpinner(){
@@ -59,59 +103,82 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-    public void doClock(){
-        mDisplayTime = (TextView) findViewById(R.id.textViewTime);
-        mDisplayTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                int hour = calendar.get(Calendar.HOUR_OF_DAY);
-                int minute = calendar.get(Calendar.MINUTE);
-                TimePickerDialog dialog = new TimePickerDialog(
-                        MainActivity.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                mDisplayTime.setText(hourOfDay + ":" + minute);
-                            }
-                        }, hour, minute, true);//Yes 24 hour time
-                dialog.setTitle("Select Time");
-                dialog.show();
-            }
-        });
+    public void doClockStart(){
+        EditText editTextTimeStart = (EditText) findViewById(R.id.editTextStartTime);
+    }
+
+    public void doClockEnd(){
+        EditText editTextEndTime = findViewById(R.id.editTextEndTime);
     }
 
     public void doCalendar(){
-
-        mDisplayDate = (TextView) findViewById(R.id.textViewDate);
-        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+        date = (EditText)findViewById(R.id.editTextDate);
+        tw = new TextWatcher() {
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
             @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-                int month = calendar.get(Calendar.MONTH);
-                int year = calendar.get(Calendar.YEAR);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        MainActivity.this,
-                        android.R.style.Theme_Material_Light_Dialog,
-                        mDateListener,
-                        day, month, year);
-                dialog.getWindow();
-                dialog.show();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
-        });
-        mDateListener = new DatePickerDialog.OnDateSetListener() {
+
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                mDisplayDate.setText(dayOfMonth + "." + (month + 1) + "." + year);
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        day  = Integer.parseInt(clean.substring(0,2));
+                        month  = Integer.parseInt(clean.substring(2,4));
+                        year = Integer.parseInt(clean.substring(4,8));
+
+                        month = month < 1 ? 1 : month > 12 ? 12 : month;
+                        cal.set(Calendar.MONTH, month-1);
+                        year = (year<1900)?1900:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d",day, month, year);
+                    }
+
+                    clean = String.format("%s.%s.%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    date.setText(current);
+                    date.setSelection(sel < current.length() ? sel : current.length());
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         };
+        date.addTextChangedListener(tw);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        getMovies();
     }
 
     @Override
@@ -119,5 +186,3 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 }
-
-
---
+++ XMLParserMovies.java
@@ -0,0 +1,116 @@
+
+package com.example.viikko9test;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.view.View;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import android.os.AsyncTask;
+
+import com.example.viikko9test.MainActivity;
+import com.example.viikko9test.Theatre;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+public class XMLParserMovies extends AsyncTask<ArrayList<String>, Void, ArrayList<String>> {
+
+    private MainActivity activity;
+    private XmlPullParserFactory factory;
+    private String url;
+
+
+    public XMLParserMovies(MainActivity activity, String url) {
+        this.activity = activity;
+        this.url = url;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+
+
+    @Override
+    protected ArrayList<String> doInBackground(ArrayList<String>... arrayLists) {
+        try {
+            ArrayList<String> movies;
+            URL myurl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) myurl.openConnection();
+            connection.setReadTimeout(10000 /* milliseconds */);
+            connection.setConnectTimeout(15000 /* milliseconds */);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream stream = connection.getInputStream();
+
+
+            factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(stream, null);
+            movies = parseXMLMovies(parser);
+            stream.close();
+            return movies;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public ArrayList<String> parseXMLMovies(XmlPullParser parser) throws IOException, XmlPullParserException {
+
+        int event;
+        ArrayList<String> movies = new ArrayList<String>();
+        String text = null;
+        event = parser.getEventType();
+
+
+        while (event != XmlPullParser.END_DOCUMENT) {
+            String name = parser.getName();
+
+            switch (event) {
+                case XmlPullParser.START_TAG:
+                    if (name.equalsIgnoreCase("Title")) {
+                        movies.add(parser.nextText()+"\n");
+                    }
+                    break;
+                case XmlPullParser.TEXT:
+                    break;
+                case XmlPullParser.END_TAG:
+                    break;
+            }
+            event = parser.next();
+
+        }
+        return movies;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<String> movies) {
+        super.onPostExecute(movies);
+        activity.callBackDataMovies(movies);
+    }
+}
+
+
+
